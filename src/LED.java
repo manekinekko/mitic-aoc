b293213d5a99c837eac992d54e6f621ec83fc1d5
sketylee@gmail.com <sketylee@gmail.com@86ead9fe-4c4d-0cb7-eb45-c36d5512da7d>
2011-10-02 15:48:59 +0000

    ammelioration de l'interface

--- src/LED.java
+++ src/LED.java
@@ -8,20 +8,22 @@ import javax.swing.JPanel;
 public class LED extends JPanel {
 	
 	private Color couleur_;
-	private String nom_;
+	private int height_;
+	private int width_;
 	
-	public LED(Color couleur, String nom){
-		
+	public LED(Color couleur){
 		couleur_ = couleur;
-		nom_ = nom;
-		setPreferredSize(new Dimension(16, 16));
+		setPreferredSize(new Dimension(width_, height_));
+		height_ = 16;
+		width_ = 16;
 	}
 	
 	public void paint(Graphics g){
 		super.paint(g);
 		g.setColor(couleur_);
-		g.fillOval(0, 0, getWidth(), getHeight());
-		
+		g.fillOval(getWidth()<=width_?0:(getWidth()-width_)/2, 
+				getHeight()<=height_?0:(getHeight()-height_)/2, 
+				width_, height_);
 	}
 
 	public Color getCouleur() {
