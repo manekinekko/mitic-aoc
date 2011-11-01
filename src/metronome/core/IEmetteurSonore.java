918dd7f151ee559aa568bae2238c435db7e3e4a8
sketylee@gmail.com <sketylee@gmail.com@86ead9fe-4c4d-0cb7-eb45-c36d5512da7d>
2011-11-01 17:04:47 +0000

    Ajout de la javadoc de la classe EmetteurSonore et de son interface

--- src/metronome/core/IEmetteurSonore.java
+++ src/metronome/core/IEmetteurSonore.java
@@ -1,7 +1,17 @@
 package metronome.core;
 
+/**
+ * Interface utilisé par EmetteurSonore. Permet d'émettre un click.
+ * 
+ * @author Wassim Chegham <contact@cheghamwassim.com>
+ * @author Gurval Le Bouter <gurval.lebouter@gmail.com>
+ * @version 1.0
+ */
 public interface IEmetteurSonore {
 
+	/**
+	 * Emet un click sonore.
+	 */
 	public void emettreClick();
 	
 }
