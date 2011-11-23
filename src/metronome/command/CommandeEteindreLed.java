12bb3293263db3a1730966bc80bd9fff3250333d
sketylee@gmail.com <sketylee@gmail.com@86ead9fe-4c4d-0cb7-eb45-c36d5512da7d>
2011-11-23 16:32:07 +0000

    suppression de code inutile, ajout de commentaires dans les classes du package command

--- src/metronome/command/CommandeEteindreLed.java
+++ src/metronome/command/CommandeEteindreLed.java
@@ -2,11 +2,25 @@ package metronome.command;
 
 import metronome.IControleur;
 
+/**
+ * Commande exécutée pour éteindre une led.
+ * 
+ * @author Wassim Chegham <contact@cheghamwassim.com>
+ * @author Gurval Le Bouter <gurval.lebouter@gmail.com>
+ * @version 1.0
+ * @see ICommand
+ */
 public class CommandeEteindreLed implements ICommand {
 
 	private IControleur controleur_;
 	private int led_;
 
+	/**
+	 * Constructeur de la commande.
+	 * @param controleur le contrôleur de l'application.
+	 * @param led le numéro de la led à éteindre.
+	 * @since 1.0
+	 */
 	public CommandeEteindreLed(IControleur controleur, int led){
 		controleur_ = controleur;
 		led_ = led;
--- src/metronome/command/MetronomeCommandeFactory.java
+++ src/metronome/command/MetronomeCommandeFactory.java
@@ -2,42 +2,94 @@ package metronome.command;
 
 import metronome.IControleur;
 
+/**
+ * Cette classe est une factory de commande.
+ * 
+ * @author Wassim Chegham <contact@cheghamwassim.com>
+ * @author Gurval Le Bouter <gurval.lebouter@gmail.com>
+ * @version 1.0
+ * 
+ */
 public class MetronomeCommandeFactory {
 
+	/**
+	 * Créer une commande CommandeTic.
+	 * @param controleur le controleur de l'application.
+	 * @return une CommandeTic.
+	 */
 	public static ICommand creerCommandeTic(IControleur controleur) {
 		return new CommandeTic(controleur);
 	}
 
+	/**
+	 * Créer une commande CommandeMarqueurTemps.
+	 * @param controleur le controleur de l'application.
+	 * @return une CommandeMarqueurTemps.
+	 */
 	public static ICommand creerCommandeMarqueurTemps(IControleur controleur) {
 		return new CommandeMarqueurTemps(controleur);
 	}
 
+	/**
+	 * Créer une commande CommandeMarqueurMesure.
+	 * @param controleur le controleur de l'application.
+	 * @return une CommandeMarqueurMesure.
+	 */
 	public static ICommand creerCommandeMarqueurMesure(IControleur controleur) {
 		return new CommandeMarqueurMesure(controleur);
 	}
 
+	/**
+	 * Créer une commande CommandeDemarrer.
+	 * @param controleur le controleur de l'application.
+	 * @return une CommandeDemarrer.
+	 */
 	public static ICommand creerCommandeDemarrer(IControleur controleur) {
 		return new CommandeDemarrer(controleur);
 	}
 
+	/**
+	 * Créer une commande CommandeStop.
+	 * @param controleur le controleur de l'application.
+	 * @return une CommandeStop.
+	 */
 	public static ICommand creerCommandeStop(IControleur controleur) {
 		return new CommandeStop(controleur);
 	}
 
+	/**
+	 * Créer une commande CommandeInc.
+	 * @param controleur le controleur de l'application.
+	 * @return une CommandeInc.
+	 */
 	public static ICommand creerCommandeInc(IControleur controleur) {
 		return new CommandeInc(controleur);
 	}
 
+	/**
+	 * Créer une commande CommandeDec.
+	 * @param controleur le controleur de l'application.
+	 * @return une CommandeDec.
+	 */
 	public static ICommand creerCommandeDec(IControleur controleur) {
 		return new CommandeDec(controleur);
 	}
 
+	/**
+	 * Créer une commande CommandeSlider.
+	 * @param controleur le controleur de l'application.
+	 * @return une CommandeSlider.
+	 */
 	public static ICommand creerCommandeSlider(IControleur controleur) {
 		return new CommandeSlider(controleur);
 	}
 
-	public static ICommand creerCommandeEteindreLed(IControleur controleur,
-			int led) {
+	/**
+	 * Créer une commande CommandeEteindreLed.
+	 * @param controleur le controleur de l'application.
+	 * @return une CommandeEteindreLed.
+	 */
+	public static ICommand creerCommandeEteindreLed(IControleur controleur, int led) {
 		return new CommandeEteindreLed(controleur, led);
 	}
 }
