f2b458b8f588675c6a0c130cdbb960e1dbb09de5
sketylee@gmail.com <sketylee@gmail.com@86ead9fe-4c4d-0cb7-eb45-c36d5512da7d>
2011-12-04 21:06:08 +0000

    maj de la javadoc

--- tags/metronome_v1.1/src/metronome/command/CommandeSlider.java
+++ tags/metronome_v1.1/src/metronome/command/CommandeSlider.java
@@ -7,11 +7,9 @@ import metronome.IControleur;
  * 
  * @author <ul>
  *         <li>Wassim Chegham (<a
- *         href="mailto:contact@cheghamwassim.com">contact
- *         @cheghamwassim.com</a>)</li>
+ *         href=mailto:contact@cheghamwassim.com>contact@cheghamwassim.com</a>)</li>
  *         <li>Gurval Le Bouter (<a
- *         href="mailto:gurval.lebouter@gmail.com">gurval
- *         .lebouter@gmail.com</a>)</li>
+ *         href=mailto:gurval.lebouter@gmail.com>gurval.lebouter@gmail.com</a>)</li>
  *         </ul>
  * @version 1.0
  * @see ICommand
@@ -23,7 +21,8 @@ public class CommandeSlider implements ICommand {
 	/**
 	 * Constructeur de la commande.
 	 * 
-	 * @param controleur le contrôleur de l'application.
+	 * @param controleur
+	 *            le contrôleur de l'application.
 	 * @since 1.0
 	 */
 	public CommandeSlider(IControleur controleur) {
